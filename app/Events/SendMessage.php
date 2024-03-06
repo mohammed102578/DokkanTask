35807910805a01cdc82ba15f6de3d02df8bbd4bf
mohammed102578 <mohammed10257883@gmail.com>
2024-03-06 12:31:56 +0200

    allfile commit

---
+++ app/Events/SendMessage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendMessage implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param string $message
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('users');
+    }
+}
